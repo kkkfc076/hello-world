d337d4ca6c728853bfb4b50e5ecbba87e4b7e1f2
kkkfc0776 <535742729@qq.com>
2020-03-07 16:15:42 +0800

    helloworld add commit

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main() {
-  printf("Hello World!\n");
-  return 0;
-}
+int main()
+{
+	printf("Hello world!\n");
+	int a,b,c;
+	a=1;
+	b=2;
+	c=a+b;
+	printf("%d",c);
+	return 0;
+ } 
